b59798c799499e62cd67e6c8653a51c94a8a0629
Viktor <sinvvvvv1@gmail.com>
2024-10-13 20:34:39 +0300

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -3,13 +3,6 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore"; // Імпорт Firestore
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBPVeZWgqA3Hu1dK7Hfx8v3BcFxw4Xu_AI",
-  authDomain: "my-place-review-app.firebaseapp.com",
-  projectId: "my-place-review-app",
-  storageBucket: "my-place-review-app.appspot.com",
-  messagingSenderId: "831815062993",
-  appId: "1:831815062993:web:f0e3bb63c46d67a1987cf0",
-  measurementId: "G-1XTEM7DZ0K",
 };
 
 const app = initializeApp(firebaseConfig);
